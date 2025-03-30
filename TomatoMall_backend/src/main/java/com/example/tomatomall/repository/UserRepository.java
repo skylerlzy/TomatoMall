442a324b57d2767d6f3c4b126161e72bbd10abaa
Cy10ne <231830096@smail.nju.edu.cn>
2025-03-30 15:38:21 +0800

    Delete TomatoMall_backend directory

--- TomatoMall_backend/src/main/java/com/example/tomatomall/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.tomatomall.repository;
-
-import com.example.tomatomall.po.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByUsername(String username);
-
-}
